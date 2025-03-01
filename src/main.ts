aa1c0eab59988f14af6f911b373d9803676cebba
N.I <iworksWeb@gmail.com>
2025-03-01 23:44:46 +0900

    add morgan acsess log src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan"; // アクセスログを出力する
 
 const PORT = 3100;
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/api/hello", (req, res) => {
   res.json({
