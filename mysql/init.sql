06d21e92a5479b5388b4f65bf634e724820b08f9
N.I <iworksWeb@gmail.com>
2025-03-03 12:06:26 +0900

    fix syntax mysql/init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,12 +4,12 @@ create database reversi;
 
 use reversi;
 
-create table games {
+create table games (
     id int primary key auto_increment,
-    started_at datetime not null,
-};
+    started_at datetime not null
+);
 
-create table turns {
+create table turns (
     id int primary key auto_increment,
     game_id int not null,
     turn_count int not null,
@@ -17,9 +17,9 @@ create table turns {
     end_at datetime not null,
     foreign key (game_id) references games(id),
     unique (game_id, turn_count)
-};
+);
 
-create table moves {
+create table moves (
     id int primary key auto_increment,
     turn_id int not null,
     disc int not null,
@@ -27,21 +27,21 @@ create table moves {
     y int not null,
     foreign key (turn_id) references turns(id),
     unique (turn_id, x, y)
-};
+);
 
-create table squares {
+create table squares (
     id int primary key auto_increment,
     turn_id int not null,
     x int not null,
     y int not null,
     disc int not null,
     foreign key (turn_id) references turns(id)
-};
+);
 
-create table game_results {
+create table game_results (
     id int primary key auto_increment,
     game_id int not null,
     winner_disc int not null,
     end_at datetime not null,
     foreign key (game_id) references games(id)
-};
+);
